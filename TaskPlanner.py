63f87c9bdc17b8ef0d98b7ef3d6d35ac2045a0bb
James Watson <james.watson-2@colorado.edu>
2025-02-07 16:45:44 -0700

    ?? JOB QUEUE ??

--- TaskPlanner.py
+++ TaskPlanner.py
@@ -7,14 +7,30 @@ from py_trees.common import Status
 
 ########## TASK PLANNER ############################################################################
 
+
+class Job:
+    def __init__( self, name, orig, data ):
+        self.name = name
+        self.orig = orig
+        self.data = data
+
+
 class TaskAgent:
     """ The Method """
 
     def __init__( self ):
         """ Init planner """
         self.status = Status.INVALID
+        self.q      = deque()
 
+
+    def run_job( self, job ):
+        pass
+
+
+    def solve_task( self ):
+        pass
     
-    
+
 
 
