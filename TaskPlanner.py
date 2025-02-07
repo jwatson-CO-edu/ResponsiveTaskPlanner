acdd8e65460b53d13ee63a512ebdb8a0531ecdba
James Watson <james.watson-2@colorado.edu>
2025-02-07 16:47:05 -0700

    NO JOBS

--- TaskPlanner.py
+++ TaskPlanner.py
@@ -8,25 +8,13 @@ from py_trees.common import Status
 ########## TASK PLANNER ############################################################################
 
 
-class Job:
-    def __init__( self, name, orig, data ):
-        self.name = name
-        self.orig = orig
-        self.data = data
-
-
 class TaskAgent:
     """ The Method """
 
     def __init__( self ):
         """ Init planner """
         self.status = Status.INVALID
-        self.q      = deque()
-
-
-    def run_job( self, job ):
-        pass
-
+        
 
     def solve_task( self ):
         pass
